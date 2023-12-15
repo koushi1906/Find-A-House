e24ab687bf4d0b8cebb120e9bb9e78df939a5a2a
koushi1906 <koushiknalam596@gmail.com>
2023-12-15 10:10:51 -0600

    complete update listing func

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -144,18 +144,20 @@ export default function Profile() {
   const handleListingDelete = async (listingId) => {
     try {
       const res = await fetch(`/api/listing/delete/${listingId}`, {
-        method: 'DELETE',
+        method: "DELETE",
       });
       const data = await res.json();
-      if(data.success === false){
+      if (data.success === false) {
         console.log(data.message);
         return;
       }
-      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
     } catch (error) {
       console.log(error.message);
     }
-  }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -248,27 +250,44 @@ export default function Profile() {
       <p className="text-red-700 mt-5">
         {showListingsError ? "Error showing listings" : ""}
       </p>
-        {userListings &&
-          userListings.length > 0 && 
-          <div className="flex flex-col gap-4">
-            <h1 className="text-center text-2xl font-semibold mt-7">Your Listings</h1>
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center text-2xl font-semibold mt-7">
+            Your Listings
+          </h1>
           {userListings.map((listing) => (
-            <div key={listing._id} className="border rounded-lg p-3 flex justify-between items-center gap-4">
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
               <Link to={`/listing/${listing._id}`}>
-                <img className="h-16 w-16 object-contain rounded-lg" src={listing.imageUrls[0]} alt="listing cover" />
+                <img
+                  className="h-16 w-16 object-contain rounded-lg"
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                />
               </Link>
-              <Link to={`/listing/${listing._id}`} className="text-slate-700 font-semibold hover:underline truncate flex-1">
+              <Link
+                to={`/listing/${listing._id}`}
+                className="text-slate-700 font-semibold hover:underline truncate flex-1"
+              >
                 <p>{listing.name}</p>
               </Link>
               <div className="flex flex-col items-center">
-                <button className="text-gray-700 uppercase">Edit</button>
-                <button onClick={() => handleListingDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-gray-700 uppercase">Edit</button>
+                </Link>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
               </div>
             </div>
           ))}
-          </div>
-        }
-          
+        </div>
+      )}
     </div>
   );
 }

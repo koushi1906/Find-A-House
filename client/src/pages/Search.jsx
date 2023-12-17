555b193d6e307e093f10013b0392cc99775c4619
koushi1906 <koushiknalam596@gmail.com>
2023-12-17 10:39:12 -0600

    main

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import { Listingitem } from '../components/Listingitem';
+import { Listingitem } from '../components/ListingItem';
 
 function Search() {
     const [sidebarData, setSidebarData] = useState({
